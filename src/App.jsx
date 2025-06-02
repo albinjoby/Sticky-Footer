9deb2457c3eb41b4db71ced6796f6f0c15396d90
Albin Joby <albinjobyponnu@gmail.com>
2025-06-02 12:48:45 +0530

    add lenis smooth scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import Lenis from 'lenis'
+import React, { useEffect } from 'react'
 import Intro from './components/Intro'
 import Footer from './components/Footer'
 
 const App = () => {
+
+  useEffect(()=>{
+    const lenis = new Lenis()
+
+    function raf(time) {
+      lenis.raf(time);
+
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  },[])
+
   return (
     <>
       <Intro /> 
